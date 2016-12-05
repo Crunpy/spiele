e978dfbdf640089615bf6035e71da41cea1f3706
Computer <Computer@192.168.0.10>
2016-12-05 16:58:50 +0100

    Another add

--- Spiele/src/fachlogik/Spiel.java
+++ Spiele/src/fachlogik/Spiel.java
@@ -1,5 +1,28 @@
 package fachlogik;
 
-public class Spiel {
+import java.util.Date;
 
+public class Spiel {
+	
+	private int id;
+	private String name;
+	private Date ge;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getGe() {
+		return ge;
+	}
+	public void setGe(Date ge) {
+		this.ge = ge;
+	}
 }
